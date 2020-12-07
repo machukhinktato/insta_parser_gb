70170edc7967418174d28e3ea479ec32f5707392
machukhinktato <tarabrinmv@gmail.com>
2020-12-07 21:28:31 +0300

    in progress

--- instaparser/spiders/instaspider.py
+++ instaparser/spiders/instaspider.py
@@ -7,6 +7,7 @@ from urllib.parse import urlencode
 from copy import deepcopy
 from .VARIABLES import *
 
+
 class InstaspiderSpider(scrapy.Spider):
     name = 'instaspider'
     allowed_domains = ['instagram.com']
@@ -18,17 +19,22 @@ class InstaspiderSpider(scrapy.Spider):
     graphql_url = 'https://www.instagram.com/qraphql/query/?'
     posts_hash = 'eddbde960fed6bde675388aac39a3657'
 
-    def parse(self, response:HtmlResponse):
+    def parse(self, response: HtmlResponse):
+        csrf = self.fetch_csrf_token(response.text)
         yield scrapy.FormRequest(
             self.insta_login_link,
             method='POST',
             callback=self.auth,
             formdata={
                 'username': self.insta_login,
-                'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1607363859:AXZQAKe4+LkKk5ihE5V9+LJiHaZiyhb/VnrzSA+XYSaXsHoDEvoUiOQlbLP5Za2zRHrGvf1cV3JNFU0TfgylqiCCsAjmFBHwgA9Jn/3fSxJmeqN8njZY38TtptDxtA6ETcKpB+2jXOqTFHRTO/Xjcg=='
-            headers={'x-csrftoken': None}
-            }
+                'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1607363859:AXZQAKe4+LkKk5ihE5V9+LJiHaZiyhb/VnrzSA+XYSaXsHoDEvoUiOQlbLP5Za2zRHrGvf1cV3JNFU0TfgylqiCCsAjmFBHwgA9Jn/3fSxJmeqN8njZY38TtptDxtA6ETcKpB+2jXOqTFHRTO/Xjcg==',
+            },
+            headers={'x-csrftoken': csrf}
         )
 
-    def auth(self, response:HtmlResponse):
-        print()
+        def auth(self, response: HtmlResponse):
+            print()
+
+        def fetch_csrf_token(self, text):
+            matched = re.search('\"csrf-token\":\"\\w+\"', text).group()
+            return matched.split(':').pop().replace(r'"', '')
