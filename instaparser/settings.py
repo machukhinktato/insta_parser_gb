d9bc286ac5767f47e03c8a7ec8d3c9ac118e1ff8
machukhinktato <tarabrinmv@gmail.com>
2020-12-10 18:58:05 +0300

    in progress

--- instaparser/settings.py
+++ instaparser/settings.py
@@ -73,7 +73,7 @@ AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
