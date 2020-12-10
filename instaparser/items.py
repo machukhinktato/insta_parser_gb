49462cb2206a5db529a830e1004c4bc5fee742f8
machukhinktato <tarabrinmv@gmail.com>
2020-12-10 18:43:04 +0300

    in progress

--- instaparser/items.py
+++ instaparser/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class InstaparserItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    user_id = scrapy.Field()
+    photo = scrapy.Field()
+    likes = scrapy.Field()
+    post_data = scrapy.Field()
