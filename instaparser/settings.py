26d24f57615ac5eed2c9e8d35eca0e3ea57f0dd7
machukhinktato <tarabrinmv@gmail.com>
2020-12-11 18:07:05 +0300

    in progress

--- instaparser/settings.py
+++ instaparser/settings.py
@@ -51,9 +51,9 @@ DOWNLOAD_DELAY = 1.25
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'instaparser.middlewares.InstaparserDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'instaparser.middlewares.InstaparserDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
