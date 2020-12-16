02b6f52b4f8abcbe988612b7375c4e2d49d0a8b6
machukhinktato <tarabrinmv@gmail.com>
2020-12-16 21:03:04 +0300

    in progress

--- instaparser/pipelines.py
+++ instaparser/pipelines.py
@@ -6,9 +6,16 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 
 class InstaparserPipeline:
+
+    def __init__(self):
+        db = MongoClient('localhost', 27017)
+        self.db = db.instagram_user_parse
+
+
     def process_item(self, item, spider):
         print()
         return item
